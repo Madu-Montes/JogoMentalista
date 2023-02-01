561805c5b6d688ccbdd3f7aca9744fafbda0f955
Madu-Montes <monteseduarda56@gmail.com>
2023-02-01 19:09:12 -0300

    finalizado 90%

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-var numSecreto = 581;
+var numSecreto = parseInt(Math.random() * 1001);
 
-var chute = prompt('Digite um número entre 1 e 1000');
+//enquanto chute diferente de numSecreto, pedir novo chute
+// != -> sinal de negação/diferente
+while (chute != numSecreto) {
+    var chute = prompt('Digite um número entre 1 e 1000');
+
+    //se chute  = ao numSecreto então imprimir o alert de Acertou
+    if (chute == numSecreto) {
+        alert('Acertou! Você é um Mentalista :)');
+    }
+    //se errado, dar dicas 
+    else if (chute > numSecreto) {
+        alert('Errou :( O número secreto é menor.');
+    }
+    else if (chute < numSecreto) {
+        alert('Errou :( O número secreto é maior.')
+    }
+}
+
+
+
+
